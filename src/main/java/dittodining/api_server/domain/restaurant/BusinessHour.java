8248e28e150020a3734ed82128e92103d019f280
falconlee236 <falconlee236@cau.ac.kr>
2025-01-02 12:42:40 +0900

    Update BusinessHour.java, DayOfWeek.java, BusinessHourConverter.java : create json base class to convert string to json

--- src/main/java/dittodining/api_server/domain/restaurant/BusinessHour.java
+++ src/main/java/dittodining/api_server/domain/restaurant/BusinessHour.java
@@ -1,7 +1,13 @@
 package dittodining.api_server.domain.restaurant;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
 public class BusinessHour {
-    private DayOfWeek dayOfWeek;
+    private DayOfWeek dayOfWeekEnum;
     private String openTime;
     private String closingTime;
     private boolean isClosedDay;
--- src/main/java/dittodining/api_server/domain/restaurant/DayOfWeek.java
+++ src/main/java/dittodining/api_server/domain/restaurant/DayOfWeek.java
@@ -1,5 +1,11 @@
 package dittodining.api_server.domain.restaurant;
 
 public enum DayOfWeek {
-    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    DAY_OF_WEEK_SUNDAY,
+    DAY_OF_WEEK_MONDAY,
+    DAY_OF_WEEK_TUESDAY,
+    DAY_OF_WEEK_WEDNESDAY,
+    DAY_OF_WEEK_THURSDAY,
+    DAY_OF_WEEK_FRIDAY,
+    DAY_OF_WEEK_SATURDAY
 }
