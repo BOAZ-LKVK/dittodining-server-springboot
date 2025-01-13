fd530c17c7dc8eb64bfaf46016ec6ee098da32a3
falconlee236 <falconlee236@cau.ac.kr>
2025-01-13 12:21:19 +0900

    ADD SelectedRestaurantRecommendation.java : create entity

---
+++ src/main/java/dittodining/api_server/domain/recommendation/SelectedRestaurantRecommendation.java
@@ -0,0 +1,43 @@
+package dittodining.api_server.domain.recommendation;
+
+import dittodining.api_server.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true)
+@Table(indexes = {
+        @Index(name = "idx_user_selected_restaurant_recommendation_m1", columnList = "created_at"),
+        @Index(name = "idx_user_selected_restaurant_recommendation_m2", columnList = "updated_at"),
+        @Index(name = "idx_user_selected_restaurant_recommendation_m3", columnList = "restaurant_recommendation_request_id"),
+        @Index(name = "idx_user_selected_restaurant_recommendation_m4", columnList = "restaurant_recommendation_id"),
+        @Index(name = "idx_user_selected_restaurant_recommendation_m5", columnList = "restaurant_id"),
+})
+public class SelectedRestaurantRecommendation extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "selected_restaurant_recommendation_id")
+    @Comment(value = "선택된 음식점 추천 ID")
+    private Long id;
+
+    @NotNull
+    @Column(name = "restaurant_recommendation_request_id")
+    @Comment(value = "음식점 추천 요청 ID")
+    private Long requestId;
+
+    @NotNull
+    @Column(name = "restaurant_recommendation_id")
+    @Comment(value = "음식점 추천 ID")
+    private Long recommendationId;
+
+    @NotNull
+    @Comment(value = "음식점 ID")
+    private Long restaurantId;
+}
