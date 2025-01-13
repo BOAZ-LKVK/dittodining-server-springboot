f0e795f0ee5cc71cdc04b4b6e6c1956fca2e75d5
falconlee236 <falconlee236@cau.ac.kr>
2025-01-13 21:58:33 +0900

    Modify RestaurantReviewRepository.java: add findByRestaurantId, findByRestaurantIdIn, CountByRestaurantId

--- src/main/java/dittodining/api_server/repository/restaurant/RestaurantReviewRepository.java
+++ src/main/java/dittodining/api_server/repository/restaurant/RestaurantReviewRepository.java
@@ -3,5 +3,13 @@ package dittodining.api_server.repository.restaurant;
 import dittodining.api_server.domain.restaurant.RestaurantReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantReviewRepository extends JpaRepository<RestaurantReview, Long> {
+
+    List<RestaurantReview> findByRestaurantId(Long restaurantId);
+
+    List<RestaurantReview> findByRestaurantIdIn(List<Long> restaurantIds);
+
+    Long countByRestaurantId(Long restaurantId);
 }
