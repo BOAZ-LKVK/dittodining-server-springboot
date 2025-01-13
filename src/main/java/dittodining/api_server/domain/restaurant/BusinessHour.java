b7c8823ccc86a943e17a8e6ceef3ac9cda4b2ac4
falconlee236 <falconlee236@cau.ac.kr>
2025-01-13 12:18:58 +0900

    Modify BusinessHour.java: add another BusinessHour properties

--- src/main/java/dittodining/api_server/domain/restaurant/BusinessHour.java
+++ src/main/java/dittodining/api_server/domain/restaurant/BusinessHour.java
@@ -1,14 +1,19 @@
 package dittodining.api_server.domain.restaurant;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter
 @ToString
+@AllArgsConstructor
 public class BusinessHour {
     private DayOfWeek dayOfWeekEnum;
     private String openTime;
     private String closingTime;
+    private String breakStartTime;
+    private String breakEndTime;
+    private String lastOrderTime;
     private boolean isClosedDay;
 }
