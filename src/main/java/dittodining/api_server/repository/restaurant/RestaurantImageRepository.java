8e10a1d48b4ea5ac6bd2a3422f422b152265b15e
falconlee236 <falconlee236@cau.ac.kr>
2025-01-13 21:54:00 +0900

    Modify RestaurantImageRepository.java: add findByRestaurantId, findByRestaurantIdIn

--- src/main/java/dittodining/api_server/repository/restaurant/RestaurantImageRepository.java
+++ src/main/java/dittodining/api_server/repository/restaurant/RestaurantImageRepository.java
@@ -3,5 +3,11 @@ package dittodining.api_server.repository.restaurant;
 import dittodining.api_server.domain.restaurant.RestaurantImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantImageRepository extends JpaRepository<RestaurantImage, Long> {
+
+    List<RestaurantImage> findByRestaurantId(Long restaurantId);
+
+    List<RestaurantImage> fndByRestaurantIdIn(List<Long> restaurantIds);
 }
