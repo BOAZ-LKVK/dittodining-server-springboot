5702733b9ac7575b0f9231db9faa769f2510dae4
falconlee236 <falconlee236@cau.ac.kr>
2025-01-13 14:20:23 +0900

    Modify RestaurantTest.java : test insert data query

--- src/test/java/dittodining/api_server/domain/restaurant/RestaurantTest.java
+++ src/test/java/dittodining/api_server/domain/restaurant/RestaurantTest.java
@@ -1,39 +1,61 @@
 package dittodining.api_server.domain.restaurant;
 
+import dittodining.api_server.repository.restaurant.RestaurantRepository;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
+@Rollback(value = true)
 class RestaurantTest {
+    @Autowired
+    private RestaurantRepository restaurantRepository;
     @Autowired
     private EntityManager em;
 
     @Test
     public void healthCheck(){
-//        Restaurant restaurant = new Restaurant(
-//                "일품진진수라 강남점",
-//                "서울 서초구 서초대로 254 지하2층",
-//                "[\"\"가족외식\"\", \"\"상견례\"\",\"\"조용한\"\", \"\"격식있는\"\", \"\"점심식사\"\", \"\"저녁식사\"\", \"\"개별룸\"\", \"\"무료주차\"\", \"\"배달\"\"]\"",
-//                130000.00,
-//                19900.00,
-//                37.49199660,
-//                127.00979130,
-//                "[{""openTime"": ""11:00"", ""closingTime"": ""21:30"", ""isClosedDay"": false, ""breakEndTime"": ""17:00"", ""dayOfWeekEnum"": ""DAY_OF_WEEK_WEDNESDAY"", ""lastOrderTime"": ""20:30"", ""breakStartTime"": ""15:00""}, {""openTime"": ""11:00"", ""closingTime"": ""21:30"", ""isClosedDay"": false, ""breakEndTime"": ""17:00"", ""dayOfWeekEnum"": ""DAY_OF_WEEK_THURSDAY"", ""lastOrderTime"": ""20:30"", ""breakStartTime"": ""15:00""}, {""openTime"": ""11:00"", ""closingTime"": ""21:30"", ""isClosedDay"": false, ""breakEndTime"": ""17:00"", ""dayOfWeekEnum"": ""DAY_OF_WEEK_FRIDAY"", ""lastOrderTime"": ""20:30"", ""breakStartTime"": ""15:00""}, {""openTime"": ""11:00"", ""closingTime"": ""21:30"", ""isClosedDay"": false, ""breakEndTime"": ""17:00"", ""dayOfWeekEnum"": ""DAY_OF_WEEK_SATURDAY"", ""lastOrderTime"": ""20:30"", ""breakStartTime"": ""15:00""}, {""openTime"": ""11:00"", ""closingTime"": ""21:30"", ""isClosedDay"": false, ""breakEndTime"": ""17:00"", ""dayOfWeekEnum"": ""DAY_OF_WEEK_SUNDAY"", ""lastOrderTime"": ""20:30"", ""breakStartTime"": ""15:00""}, {""openTime"": ""11:00"", ""closingTime"": ""21:30"", ""isClosedDay"": false, ""breakEndTime"": ""17:00"", ""dayOfWeekEnum"": ""DAY_OF_WEEK_MONDAY"", ""lastOrderTime"": ""20:30"", ""breakStartTime"": ""15:00""}, {""openTime"": ""11:00"", ""closingTime"": ""21:30"", ""isClosedDay"": false, ""breakEndTime"": ""17:00"", ""dayOfWeekEnum"": ""DAY_OF_WEEK_TUESDAY"", ""lastOrderTime"": ""20:30"", ""breakStartTime"": ""15:00""}]"
-//        );
-//        em.persist(restaurant);
-//
-//        Restaurant findRestaurant = em.find(Restaurant.class, restaurant.getId());
-//        assertEquals(restaurant, findRestaurant);
-//        System.out.println(org.hibernate.Version.getVersionString());
-    }
-    /*
-    ",,,4.05,4.44,842,3.20,19,863,2024-10-14 05:39:08,2024-10-14 05:39:08,4.50,2,0507-1394-3112
+        List<BusinessHour> businessHours = List.of(
+                new BusinessHour(DayOfWeek.DAY_OF_WEEK_WEDNESDAY, "11:00", "21:30", "15:00", "17:00", "20:30", false),
+                new BusinessHour(DayOfWeek.DAY_OF_WEEK_THURSDAY, "11:00", "21:30", "15:00", "17:00", "20:30", false),
+                new BusinessHour(DayOfWeek.DAY_OF_WEEK_FRIDAY, "11:00", "21:30", "15:00", "17:00", "20:30", false),
+                new BusinessHour(DayOfWeek.DAY_OF_WEEK_SATURDAY, "11:00", "21:30", "15:00", "17:00", "20:30", false),
+                new BusinessHour(DayOfWeek.DAY_OF_WEEK_SUNDAY, "11:00", "21:30", "15:00", "17:00", "20:30", false),
+                new BusinessHour(DayOfWeek.DAY_OF_WEEK_MONDAY, "11:00", "21:30", "15:00", "17:00", "20:30", false),
+                new BusinessHour(DayOfWeek.DAY_OF_WEEK_TUESDAY, "11:00", "21:30", "15:00", "17:00", "20:30", false)
+        );
+        Restaurant restaurant = new Restaurant(
+                "일품진진수라 강남점",
+                "서울 서초구 서초대로 254 지하2층",
+                "[가족외식, 상견례,조용한, 격식있는, 점심식사, 저녁식사, 개별룸, 무료주차, 배달]",
+                new BigDecimal("130000.00"),
+                new BigDecimal("19900.00"),
+                new BigDecimal("37.49199660"),
+                new BigDecimal("127.00979130"),
+                businessHours,
+                new BigDecimal("4.05"),
+                new BigDecimal("4.44"),
+                842L,
+                new BigDecimal("3.20"),
+                19L,
+                863L
+        );
+        restaurantRepository.save(restaurant);
 
-     */
+        Restaurant findRestaurant = restaurantRepository.findById(restaurant.getId()).get();
+        assertEquals(restaurant, findRestaurant);
+        System.out.println(org.hibernate.Version.getVersionString());
+    }
 }
