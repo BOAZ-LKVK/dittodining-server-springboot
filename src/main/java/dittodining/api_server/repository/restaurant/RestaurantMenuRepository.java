d74c100e4af8803afaa947ec1ba2ac8eb568a9fc
falconlee236 <falconlee236@cau.ac.kr>
2025-01-13 21:55:33 +0900

    Modify RestaurantMenuRepository.java: add findByRestaurantId, findByRestaurantIdIn

--- src/main/java/dittodining/api_server/repository/restaurant/RestaurantMenuRepository.java
+++ src/main/java/dittodining/api_server/repository/restaurant/RestaurantMenuRepository.java
@@ -3,5 +3,11 @@ package dittodining.api_server.repository.restaurant;
 import dittodining.api_server.domain.restaurant.RestaurantMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantMenuRepository extends JpaRepository<RestaurantMenu, Long> {
+
+    List<RestaurantMenu> findByRestaurantId(Long restaurantId);
+
+    List<RestaurantMenu> findByRestaurantIdIn(List<Long> restaurantIds);
 }
