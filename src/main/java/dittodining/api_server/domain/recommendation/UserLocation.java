b900e163b566c0fb67ba174c25288d1179c13ff2
falconlee236 <falconlee236@cau.ac.kr>
2025-01-13 15:10:05 +0900

    Modify UserLocation.java : add All args constructor for create Object correctly and to string annotation for debugging

--- src/main/java/dittodining/api_server/domain/recommendation/UserLocation.java
+++ src/main/java/dittodining/api_server/domain/recommendation/UserLocation.java
@@ -3,13 +3,17 @@ package dittodining.api_server.domain.recommendation;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.Comment;
 
 import java.math.BigDecimal;
 
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserLocation {
     @NotNull
     @Column(precision = 11, scale = 8)
