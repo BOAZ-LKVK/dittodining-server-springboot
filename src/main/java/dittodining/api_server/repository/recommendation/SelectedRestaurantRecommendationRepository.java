f27308b103c8a82ce67d55bdb2a663185ee52f7a
falconlee236 <falconlee236@cau.ac.kr>
2025-01-14 19:23:16 +0900

    Create SelectedRestaurantRecommendationRepository.java: add repositories

--- src/main/java/dittodining/api_server/repository/recommendation/SelectedRestaurantRecommendationRepository.java
+++ src/main/java/dittodining/api_server/repository/recommendation/SelectedRestaurantRecommendationRepository.java
@@ -3,5 +3,9 @@ package dittodining.api_server.repository.recommendation;
 import dittodining.api_server.domain.recommendation.SelectedRestaurantRecommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SelectedRestaurantRecommendationRepository extends JpaRepository<SelectedRestaurantRecommendation, Long> {
+
+    List<SelectedRestaurantRecommendation> findByRequestId(Long requestId);
 }
