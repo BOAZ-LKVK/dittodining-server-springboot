8f8891eee2f5c3c565ad7132792278a4d7349cfe
falconlee236 <falconlee236@cau.ac.kr>
2025-01-14 19:35:13 +0900

    Modify HealthCheckApi.java: Create health check

--- src/main/java/dittodining/api_server/api/HealthCheckApi.java
+++ src/main/java/dittodining/api_server/api/HealthCheckApi.java
@@ -1,12 +1,12 @@
-package dittodining.api_server.controller;
+package dittodining.api_server.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @GetMapping("/hello")
+public class HealthCheckApi {
+    @GetMapping("/health")
     public String hello() {
-        return "Hello World";
+        return "Health Check OK";
     }
 }
